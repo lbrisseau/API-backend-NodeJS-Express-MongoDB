0c4ff5abe330711c0b6d901daaa683a41178f27f
Lionel BRISSEAU <lbrisseau@korben.com>
2021-09-03 15:55:14 +0200

    functional API

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Product = require('./models/Product');
+
+mongoose.connect('mongodb+srv://lionel:LKyeV343JQkV3jIQ@cluster0.svgkd.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/products', (req, res, next) => {
+    delete req.body._id;
+    const product = new Product({
+        ...req.body
+    });
+    product.save()
+        .then(product => res.status(201).json({product}))
+        .catch(error => res.status(400).json({error}));
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne( {_id: req.params.id} )
+        .then(oneResult => res.status(200).json({ product: oneResult }))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        .then(list => res.status(200).json({ products: [...list] }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Modified!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Deleted!'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
